0fdd6497d878df997a20056ab183e0f69901d916
Carlotadiez <145040309+Carlotadiez@users.noreply.github.com>
2023-11-06 18:21:38 +0100

    js architects + correción css home + foto projects

---
+++ js/architects.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    
+
+    $("#menu i.fa-bars").click(function(){
+        
+        $("#menu ul").css("left", "0%");
+    });
+
+
+    $("#menu ul li").click(function(){
+        
+        $("#menu ul").css("left", "-100%");
+    });
+
+    $(window).scroll(function() {    
+        var scrollTop = $(this).scrollTop();
+        console.log(scrollTop);
+
+        var windowHeight = $(window).height();
+
+        var menuHeight = $("#menu").height();
+
+        var maxScroll = windowHeight - menuHeight;
+        console.log("max " + maxScroll);
+
+        var opacity = scrollTop / maxScroll;
+
+        $("#menu").css("background-color", "rgba(0, 0, 0, " + opacity + ")");
+    });
+
+
+
+});
